e081ca0b16dacb0bef3943f2f1ff2f30ef569e3a
Matt Clarke <matt.clarke@esss.se>
2020-09-04 10:52:07 +0200

    Separated out image creation and jpeg saving

--- main.py
+++ main.py
@@ -41,27 +41,31 @@ def get_ad_image_data(prefix):
     return raw, xsize, ysize
 
 
-def convert_to_jpg(data):
+def convert_to_resized_image(data):
+    image = Image.frombuffer("L", (data[1], data[2]), data[0], "raw", "L", 0, 1)
+    if image.width > MAX_SIZE or image.height > MAX_SIZE:
+        if image.width > image.height:
+            new_size = (MAX_SIZE, MAX_SIZE * int(image.height / image.width))
+        else:
+            new_size = (MAX_SIZE * int(image.width / image.height), MAX_SIZE)
+        image = image.resize(new_size, Image.BOX)
+    return image
+
+
+def save_image_as_jpeg(image):
     # Create the images folder if it doesn't exist
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
-
     path = f"{OUTPUT_DIR}out-{int(time.time())}.jpg"
-    img = Image.frombuffer("L", (data[1], data[2]), data[0], "raw", "L", 0, 1)
-    if img.width > MAX_SIZE or img.height > MAX_SIZE:
-        if img.width > img.height:
-            new_size = (MAX_SIZE, MAX_SIZE * int(img.height / img.width))
-        else:
-            new_size = (MAX_SIZE * int(img.width / img.height), MAX_SIZE)
-        img = img.resize(new_size, Image.BOX)
-    img.save(path, "JPEG", quality=QUALITY)
+    image.save(path, "JPEG", quality=QUALITY)
     return os.path.abspath(path)
 
 
 if __name__ == "__main__":
     image_data = get_ad_image_data("13SIM1:image1")
-    filepath = convert_to_jpg(image_data)
+    img = convert_to_resized_image(image_data)
+    save_image_as_jpeg(img)
 
-    photo = plt.imread(filepath)
-    plt.imshow(photo)
+    plt.imshow(img)
+    plt.colorbar()
     plt.show()
