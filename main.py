33f702d63b3099919ef69f145482d6b20db1d194
Matt Clarke <matt.clarke@esss.se>
2020-09-07 13:55:01 +0200

    Use p4p by default

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import time
 import matplotlib.pyplot as plt
 from epics import PV
 from matplotlib.animation import FuncAnimation
+from p4p.client.thread import Context
 from PIL import Image
 
 MAX_SIZE = 512
 QUALITY = 80
 OUTPUT_DIR = "./images/"
+P4P_CONTEXT = Context("pva")
 
 
 class PvNotFoundException(Exception):
@@ -36,15 +38,28 @@ def caget(name, as_str=False):
         raise PvGetException(f"Unable to get value for PV {name}: {err}")
 
 
-def get_ad_image_data(prefix):
-    raw = caget(f"{prefix}:ArrayData")
-    xsize = caget(f"{prefix}:ArraySize0_RBV")
-    ysize = caget(f"{prefix}:ArraySize1_RBV")
-    return raw, xsize, ysize
+def get_ad_image_data(prefix, use_ca=False):
+    if use_ca:
+        get_pv = caget
+    else:
+        get_pv = P4P_CONTEXT.get
+
+    raw = get_pv(f"{prefix}:ArrayData")
+    xsize = get_pv(f"{prefix}:ArraySize0_RBV")
+    ysize = get_pv(f"{prefix}:ArraySize1_RBV")
+    zsize = get_pv(f"{prefix}:ArraySize2_RBV")
+    colour_mode = get_pv(f"{prefix}:ColorMode_RBV")
+    return raw, colour_mode, [xsize, ysize, zsize]
 
 
 def convert_to_resized_image(data):
-    image = Image.frombuffer("L", (data[1], data[2]), data[0], "raw", "L", 0, 1)
+    buffer, colour_mode, sizes = data
+    if colour_mode == 0:  # Mono
+        image = Image.frombuffer("L", (sizes[0], sizes[1]), buffer, "raw", "L", 0, 1)
+    elif colour_mode == 2:
+        image = Image.frombuffer("P", (sizes[1], sizes[2]), buffer, "raw", "P", 0, 1)
+    else:
+        raise Exception(f"Cannot handle {colour_mode}")
     if image.width > MAX_SIZE or image.height > MAX_SIZE:
         if image.width > image.height:
             new_size = (MAX_SIZE, MAX_SIZE * int(image.height / image.width))
@@ -74,6 +89,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s", "--save", action="store_true", help="save the image as jpeg"
     )
+
+    parser.add_argument("-c", "--ca", action="store_true", help="use channel access")
+
     args = parser.parse_args()
 
     if args.animation_mode:
@@ -81,17 +99,17 @@ if __name__ == "__main__":
         fig, _ = plt.subplots()
 
         def update(frame):
-            raw_image_data = get_ad_image_data("13SIM1:image1")
+            raw_image_data = get_ad_image_data("13SIM1:image1", args.ca)
             img = convert_to_resized_image(raw_image_data)
             return plt.imshow(img)
 
         animation = FuncAnimation(fig, update, interval=500)
         plt.show()
     else:
-        raw_image_data = get_ad_image_data("13SIM1:image1")
+        raw_image_data = get_ad_image_data("13SIM1:image1", args.ca)
         img = convert_to_resized_image(raw_image_data)
-        if args.save:
-            save_image_as_jpeg(img)
+        # if args.save:
+        #     save_image_as_jpeg(img)
 
         image_plot = plt.imshow(img)
         plt.colorbar()
